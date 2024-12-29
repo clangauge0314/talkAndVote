331640942026bae0e865ecd10f299398de431acd
pipil <jmh4011@gmail.com>
2024-12-29 12:58:58 +0900

    model seleted

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,19 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-<<<<<<< HEAD
-class User(models.Model):
-    id = models.AutoField(primary_key=True, unique=True)
-    username = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=128)
-    dateOfBirth = models.DateField(blank=True, null=True)
-=======
 
 class User(AbstractUser):
 
     date_of_birth = models.DateField(blank=True, null=True)
->>>>>>> bf8d4388f71d6158feb90f7e93a5f177de90a326
     gender = models.CharField(
         max_length=10,
         choices=[("male", "Male"), ("female", "Female")],
